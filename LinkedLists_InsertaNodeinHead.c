c12c025ddc7bd001673757f2db2b35bd694a9c8f
yselimkaya <yselimkaya0@gmail.com>
2025-05-23 11:23:01 +0300

    Delete LinkedLists_InsertaNodeinHead.c

--- LinkedLists_InsertaNodeinHead.c
+++
@@ -1,8 +0,0 @@
-SinglyLinkedListNode* insertNodeAtHead(SinglyLinkedListNode* llist, int data) {
-    SinglyLinkedListNode* temp = llist;
-    SinglyLinkedListNode* newNode = (SinglyLinkedListNode*)malloc(sizeof(SinglyLinkedListNode));
-    newNode->data = data;
-    llist = newNode;
-    newNode->next = temp;
-    return newNode;
-}
